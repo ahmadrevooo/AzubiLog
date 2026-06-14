3d88dbb79590eab060759db0ce3e07dbe9b331d8
ahmadrevooo <ahmed.almohammedd@gmail.com>
2026-06-14 17:32:31 +0200

    I added the models or classes

---
+++ Models/Ausbilder.cs
@@ -0,0 +1,12 @@
+﻿namespace AzubiLog.Models
+{
+    public class Ausbilder
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Abteilung { get; set; } = "";
+
+        public List<BerichtEintrag> BerichtEintrag { get; set; } = new();
+    }
+}
