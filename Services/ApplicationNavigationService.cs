9fb8151914b8c15219cdf9573ac2b3e81d95428b
ahmadrevooo <ahmed.almohammedd@gmail.com>
2026-07-02 01:45:43 +0200

    Add timetable times and flexible school entry mode and removed Timetable from navi to profile dropdown

--- Services/ApplicationNavigationService.cs
+++ Services/ApplicationNavigationService.cs
@@ -8,7 +8,6 @@ public class ApplicationNavigationService : IApplicationNavigationService
         new("NavNewEntry", "report-entries/new", "bi-plus-square-fill-nav-menu", true),
         new("NavWeeklyReports", "weekly-reports/grid", "bi-journal-text-nav-menu", true),
         new("NavTodos", "todos", "bi-list-nested-nav-menu", true),
-        new("TimetableNavItem", "timetable", "bi-calendar-week-nav-menu", true),
         new("NavSettings", "settings", "bi-gear-fill-nav-menu", true)
     ];
 
